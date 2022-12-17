7012c3e67c003d25be192441761830f5c3f3385d
unknown <yasiruranpatabendige98@gmail.com>
2022-12-17 20:54:16 +0530

    initial commit

--- nirwana_web/assets/js/My-Date-Picker.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-        $('#datetimepicker1').datepicker({
-            format: "dd/mm/yyyy",
-            language: "es",
-            autoclose: true,
-            todayHighlight: true
-        });
-    });
